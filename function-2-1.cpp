b661120cc5435d9267bfe481198e14449cee5449
Torin Cooper <torincooper@student-10-201-07-093.wireless.adelaide.edu.au>
2024-03-08 17:01:34 +1030

    Bug fixes in 2-1

--- function-2-1.cpp
+++ function-2-1.cpp
@@ -15,5 +15,14 @@ void print_binary_str(std::string decimal_number) {
     num = (int)floor((float)num / 2.0);
   }
 
-  std::cout << out << std::endl;
+  int reverse = 0;
+  int remainder;
+
+  while (out != 0) {
+    remainder = out % 10;
+    reverse = reverse * 10 + remainder;
+    out /= 10;
+  }
+
+  std::cout << reverse << std::endl;
 }
